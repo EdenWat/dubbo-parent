86c182c544c601f19805149cb6022ef8b50cd262
wangj <wangj@13322.com>
2018-03-13 16:00:19 +0800

    添加工具类

--- mould-core/src/main/java/com/app/core/util/GZipUtil.java
+++ mould-core/src/main/java/com/app/core/util/GZipUtil.java
@@ -3,10 +3,10 @@ package com.app.core.util;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.codec.CharEncoding;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -15,7 +15,7 @@ public abstract class GZipUtil {
     /**
      * 压缩GZip
      *
-     * @return
+     * @return String
      */
     public static String gZip(String input) {
         byte[] bytes = null;
@@ -32,8 +32,14 @@ public abstract class GZipUtil {
         } catch (Exception e) {
             log.error("压缩出错：", e);
         } finally {
-            IOUtils.closeQuietly(gzip);
-            IOUtils.closeQuietly(bos);
+            try {
+                if (gzip != null)
+                    gzip.close();
+                if (bos != null)
+                    bos.close();
+            } catch (final IOException ioe) {
+                log.error("压缩出错：", ioe);
+            }
         }
         return Base64.encodeBase64String(bytes);
     }
@@ -41,19 +47,19 @@ public abstract class GZipUtil {
     /**
      * 解压GZip
      *
-     * @return
+     * @return String
      */
     public static String unGZip(String input) {
-        byte[] bytes = null;
+        byte[] bytes;
         String out = input;
         GZIPInputStream gzip = null;
-        ByteArrayInputStream bis = null;
+        ByteArrayInputStream bis;
         ByteArrayOutputStream bos = null;
         try {
             bis = new ByteArrayInputStream(Base64.decodeBase64(input));
             gzip = new GZIPInputStream(bis);
             byte[] buf = new byte[1024];
-            int num = -1;
+            int num;
             bos = new ByteArrayOutputStream();
             while ((num = gzip.read(buf, 0, buf.length)) != -1) {
                 bos.write(buf, 0, num);
@@ -67,8 +73,14 @@ public abstract class GZipUtil {
         } catch (Exception e) {
             log.error("解压出错：", e);
         } finally {
-            IOUtils.closeQuietly(gzip);
-            IOUtils.closeQuietly(bos);
+            try {
+                if (gzip != null)
+                    gzip.close();
+                if (bos != null)
+                    bos.close();
+            } catch (final IOException ioe) {
+                log.error("解压出错：", ioe);
+            }
         }
         return out;
     }
